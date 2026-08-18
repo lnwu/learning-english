4daedc719a81e5eac6fc61377f59b5a2f7c6ca58
lnwu <wind2729@gmail.com>
2026-08-19 06:00:18 +0800

    feat: add Vercel Speed Insights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./index.css";
 import { Inter, Noto_Sans_SC } from "next/font/google";
 import { headers } from "next/headers";
@@ -39,6 +40,7 @@ const RootLayout: FC<{ children: ReactNode }> = async ({ children }) => {
           </WordsProvider>
         </AuthProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
